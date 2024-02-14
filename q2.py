cd2e1fd193184eb1432ddfc6963e328477f050a6
jngu36 <42044445+jngu36@users.noreply.github.com>
2024-02-13 20:18:31 -0500

    Update q2.py

--- q2.py
+++ q2.py
@@ -12,16 +12,37 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def graphSnowfall(t):
-    bars = [0,0,0,0,0]  # data structure to hold the values (for this test case, the numbers are from 0 to 50)
+    bars = {}  # data structure to hold the values (for this test case, the numbers are from 0 to 50)
     f = open(t, "r") #open file
     for line in f.readlines(): # reading line by line
-        value = int(line) - 1 # to make the next line easier to read, grabbing the value and rounding it down
-        bars[int((value)/10)]+= 1
+        value = int((int(line) - 1)/10) # to make the next line easier to read, grabbing the value and rounding it down
+        if value in bars:
+            bars[value] +=1
+        else:
+            bars[value] = 1
         # by shifting the number down by one and rounding down, the numbers can work themselves
         # into an index value to increment the appropriate slot in the array
     f.close() #close file
-    
-    x = np.array(["0-10cm", "11-20cm", "21-30cm", "31-40cm", "41-50cm"]) #X-axis label
-    y = np.array(bars) # the y value 
+
+    # a whole ton of converting
+
+    #generating x labels
+    x_label = []
+    for keys in bars:
+        if(keys == 0):
+            x_label.append("0-10")
+        else:
+            x_label.append(str((keys * 10)+1) + " - " + str((keys+1) * 10))
+            
+    # since bars is a dictionary and plot needs an array, generate an array
+    # each value corresponds to each label
+            
+    y_values = []
+    for y in bars:
+        y_values.append(bars[y])
+
+    # plot the things
+    x = np.array(x_label) #X-axis label
+    y = np.array(y_values) # the y value 
     plt.bar(x,y) #plotting the bars
-    plt.show() #showing the bar grapgh
+    plt.show() #showing the bar graph
